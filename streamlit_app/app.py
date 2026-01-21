86e198ca14ef9ea75f76340c8fa7dc460aac305b
nimit <n.kochar.newdelhi@gmail.com>
2026-01-21 07:11:41 +0530

    app.py updated with import of path

--- streamlit_app/app.py
+++ streamlit_app/app.py
@@ -3,6 +3,9 @@ import pandas as pd
 import plotly.express as px
 import plotly.graph_objects as go
 from datetime import datetime
+from pathlib import Path
+# Resolve project root directory safely (works locally + cloud)
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 # ---------------- PAGE CONFIG ----------------
 st.set_page_config(
@@ -147,7 +150,7 @@ st.markdown("""
 col1, col2 = st.columns([0.15, 0.85])
 
 with col1:
-    st.image("../assets/download.png", width=130)
+    st.image("BASE_DIR/assets/download.png", width=130)
 
 with col2:
     st.markdown("<h1 style='margin-bottom:0;'>Ola Analytics & Forecast Portal</h1>", unsafe_allow_html=True)
@@ -199,7 +202,7 @@ with tab2:
     """, unsafe_allow_html=True)
 
     try:
-        file_path = "../forecasting/forecast_output.csv"
+        file_path = "BASE_DIR/forecasting/forecast_output.csv"
         df = pd.read_csv(file_path)
         df['booking_date'] = pd.to_datetime(df['booking_date'])
         df['revenue_millions'] = df['forecast_revenue'] / 1_000_000
